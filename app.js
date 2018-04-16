49deaafe379555fbd8fb487a152f98925adf0b80
Maryann Ullrich <maryann.ullrich@gmail.com>
2018-04-16 12:05:16 -0500

    Lab-5-jQuery-2-Final

--- app.js
+++ app.js
@@ -23,15 +23,17 @@ $(document).ready(function () {
         incorrectLetterCounter = 0;
         correctLetterCounter = 0;
         sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
-        'Too ato too nOt enot one totA not anot tOO aNot',
-        'oat itain oat tain nate eate tea anne inant nean',
-        'itant eate anot eat nato inate eat anot tain eat',
-        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+            'Too ato too nOt enot one totA not anot tOO aNot',
+            'oat itain oat tain nate eate tea anne inant nean',
+            'itant eate anot eat nato inate eat anot tain eat',
+            'nee ene ate ite tent tiet ent ine ene ete ene ate'];
         sentenceContent = sentences[0];
         reachedEnd = false;
         startTimer = 0;
         endTimer = 0;
         highLightCharacter();
+
+
     }
 
     document.addEventListener("keydown", function (event) {
@@ -105,6 +107,20 @@ $(document).ready(function () {
         if (numCharsTyped == sentenceContent.length) {
             reachedEnd = true;
             endTimer = event.timeStamp;
+
+            alert("You Typed " + getWordsPerMinute() + " Words Per Minute!");
+
+            myFunction();
+
+            function myFunction() {
+                let txt;
+                let r = confirm("Do You Want To Play This Typing Game Again?");
+                if (r == true) {
+                    initialize();
+                } else {
+                    txt = "Thank You For Playing!";
+                }
+            }
         }
     });
 
@@ -129,7 +145,6 @@ $(document).ready(function () {
         highContent = highContent.substr(0, numCharsTyped) +
             rep + highContent.substr(numCharsTyped + 1);
         $('#sentence').html(highContent);
-        // $("#target-letter").html(nextLetter);
     }
 
     function getWordsPerMinute() {
@@ -137,58 +152,11 @@ $(document).ready(function () {
         let min = Math.floor(timer / 60000);
         let sec = Math.floor((timer % 60000) / 1000);
         let time = min + sec / 60;
-        return Math.floor((48 - incorrectLetterCounter) / time);
+        let words = sentenceContent.split(/\s+/);
+        return Math.floor(words.length / time);
+
     }
 
     initialize();
 });
-
-
-
-
-// let startTimer;
-// let endTimer;
-// let error = 0;
-
-// function getWPM(){                              
-//     let timer = endTimer - startTimer;
-//     let min = Math.floor(timer/60000);
-//     let sec = Math.floor((timer%60000)/1000);
-//     let time = min + sec/60;
-//     return Math.floor((48 - error)/time);
-// }
-
-
-// $(document).keypress(function(e){
-//     ascii = e.which;
-//     $('#'+ ascii).addClass('keypress');
-
-//     if(!gameOver){
-//         if(counter === 0 && line === 0){
-//             startTimer = e.timeStamp;
-//         }else if (line === 4 && counter === sentences[line].length - 1){
-//             endTimer = e.timeStamp;
-//         }
-
-//         checkLetter();
-
-//         if (counter + 1 < sentences[line].length){
-//             counter++;
-//         }else if(counter + 1 >= sentences[line].length && line < 4){
-//             line++;
-//             init(false);
-//         }else {
-//             $('.key').removeClass('keypress');
-//             $('#feedback').text('You typed ' + getWPM() + ' wpm.  Great Job!');
-//             setTimeout(function(){
-//                 var again = confirm('Would you like to try again?');
-//                 if (again) {
-//                     init(true);
-//                 } else {
-//                     gameOver = true;
-//                 }
-//             },2000);
-//         }
-//     }
-// })
 
